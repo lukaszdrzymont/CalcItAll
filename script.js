dd128c0b04070b99f98b39c68cdc62588e0a40f6
Lukasz Drzymont <lukaszdrzymont@gmail.com>
2023-06-20 22:14:36 +0200

    fixed js,  addEventListers and started to adding logic to function convertUnits

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-class Tag{
-    constructor(tagName,id,content) {
-        this.tagName=tagName,
-        this.id = id,
-        this.content= content
-    }
-    insertTag(parentId,tagObj) {
-        let tag = document.createElement(tagObj.tagName);
-        tag.id = tagObj.id;
-        tag.textContent = tagObj.content;
-        document.getElementById(parentId).appendChild(tag)
-    }
+let number;
+let whatPickedFrom;
+let whatPickedTo;
 
-}
 let buttonsFromTo = '<ul><li><button type="button" class="buttonix newButton" id="button-meter">Meter</button></li><li><button type="button" class="buttonix newButton" id="button-kilometer">Kilometer</button></li><li><button type="button" class="buttonix newButton" id="button-centimeter">Centimeter</button></li><li><button type="button" class="buttonix newButton" id="button-mile">Mile</button></li></ul>'
-document.getElementById('button-length').addEventListener('click', function(){
-    document.getElementById('convert-from').innerHTML = "From:"+buttonsFromTo
-    
-})
-document.getElementById("convert-from").addEventListener('click', function(){
-    if(event.target.classList.contains('newButton')){
-         document.getElementById('convert-to').innerHTML = "To:"+buttonsFromTo
-         let whatPickedFrom = event.target.id
-         console.log(whatPickedFrom)
+
+document.getElementById('button-length').addEventListener('click', function() {
+    document.getElementById('convert-from').innerHTML = "From:" + buttonsFromTo
+    document.getElementById('convert-to').innerHTML = "To:" + buttonsFromTo
+});
+
+document.addEventListener('click', function(event){
+    if(event.target.classList.contains('newButton')) {
+        if(event.target.parentElement.parentElement.parentElement.id === 'convert-from') {
+            whatPickedFrom = event.target.id;
+            ///document.getElementById(whatPickedFrom).classList.add('selected');
+            console.log(whatPickedFrom);
+        }
+        if(event.target.parentElement.parentElement.parentElement.id === 'convert-to') {
+            whatPickedTo = event.target.id;
+            //document.getElementById(whatPickedTo).classList.add('selected');
+            console.log(whatPickedTo);
+        }
     }
-        })
-    
-document.getElementById("convert-to").addEventListener('click', function(){
-    if(event.target.classList.contains('newButton')){
-        let whatPickedTo = event.target.id;
-        console.log(whatPickedTo)
-    }
-})
-document.getElementById("button-convert").addEventListener('click',function(){
-    let number = document.getElementById('unit-input').value;
-    console.log(number)
-})
+});
+
+document.getElementById("button-convert").addEventListener('click', function(){
+    number = document.getElementById('unit-input').value;
+    console.log(number);
+
+    let result = convertUnits(whatPickedFrom, whatPickedTo, number);
+    document.getElementById('asasa').innerHTML = result;
+});
 
 function convertUnits(from, to, number) {
-    let meter= 1;
-    let mile = 0.0006213712
-    let kilometer = 0.001;
-    let centimeter =100
+    let result;
+    if(from === 'button-meter' && to === 'button-kilometer') {
+        result = number * 0.001;
+    }
+    else if(from === 'button-meter' && to === 'button-mile') {
+        result = number * 0.000621371192;
+    }
+
+    return result;
 }
 
+    
